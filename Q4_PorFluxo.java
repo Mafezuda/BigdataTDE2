3a7c846b5dc905aee56effdfffcf52a867027c1b
Maria Fernanda <162508333+Mafezuda@users.noreply.github.com>
2025-09-20 17:43:42 -0300

    Create Q4_PorFluxo.java

---
+++ Q4_PorFluxo.java
@@ -0,0 +1,18 @@
+public class Q4_PorFluxo {
+    public static class Map extends Mapper<LongWritable, Text, Text, LongWritable>{
+        private static final LongWritable ONE = new LongWritable(1);
+        @Override
+        protected void map(LongWritable k, Text v, Context c) throws IOException, InterruptedException {
+            String[] f = v.toString().split(";");
+            if (f[0].equalsIgnoreCase("Country") || f.length<10) return;
+            c.write(new Text(f[4]), ONE);
+        }
+    }
+    public static class Reduce extends Reducer<Text, LongWritable, Text, LongWritable>{
+        @Override
+        protected void reduce(Text k, Iterable<LongWritable> vals, Context c) throws IOException, InterruptedException {
+            long soma=0; for(LongWritable v:vals) soma+=v.get();
+            c.write(k,new LongWritable(soma));
+        }
+    }
+}
